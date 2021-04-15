25f5645f618097999bb8d23680cddec12b284b2c
Eric Dowd <dowderic@hotmail.com>
2021-04-14 22:26:54 -0400

    .

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,9 +1,5 @@
 // @TODO: YOUR CODE HERE!
 
-// import data from CSV
-d3.csv("../assets/data/data.csv").then(function(data) {
-  console.log(data);
-});
 
 
 // creating dimensions for scatter plot graph
@@ -23,3 +19,35 @@ var svg = d3.select("#scatter")
   .append("g")
   .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+
+// import data from CSV
+d3.csv("../assets/data/data.csv").then(function(data) {
+  console.log(data);
+});
+
+
+    // Adding X axis with Poverty Data
+  var x = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Adding Y axis with Lacks Healthcare %
+  var y = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(data.poverty); } )
+      .attr("cy", function (d) { return y(data.healthcareLow); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
