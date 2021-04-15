637a454dc0790864ec44102cea9841f8bb534571
Eric Dowd <dowderic@hotmail.com>
2021-04-14 22:03:08 -0400

    .

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,3 +6,20 @@ d3.csv("../assets/data/data.csv").then(function(data) {
 });
 
 
+// creating dimensions for scatter plot graph
+
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+
+
+// attaching on the scatter object to the main page
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
